c753661702404c1b94747e488419f3dd70cab047
cmerino9 <57924005+cmerino9@users.noreply.github.com>
2020-03-23 19:17:17 -0400

    Add files via upload

--- Project1-MysteryInc.py
+++ Project1-MysteryInc.py
@@ -5,9 +5,34 @@ Created on Sat Mar 21 08:43:34 2020
 @authors: Cortney, Thuy, Shireen, Kazi
 """
 
-employmentData = open("TotalPrivateEmployment-data.csv", "r") #data file and program are in same directory
-for aline in employmentData:
-    values = aline.split() #we need to figure out where to split this data
-    print(values)
-    
-employmentData.close()
+'''employmentData = open("TotalPrivateEmployment-data.csv", "r") #data file and program are in same directory4
+lineList = [line.rstrip('\n') for line in open("TotalPrivateEmployment-data.csv")]
+'''
+import csv
+with open('TotalPrivateEmployment-data.csv', mode='r') as csv_file:
+    csv_reader = csv.DictReader(csv_file, delimiter=',')
+    line_count = 0
+    for row in csv_reader:
+        if line_count == 0:
+            print(f'Column names are {", ".join(row)}')
+            line_count += 1
+        print(row)
+        line_count += 1
+    print(f'Processed {line_count} lines.')
+
+President_file = open('Presidents.txt','r')
+president=[]
+parties=[]
+for line in President_file:
+    tmp_list = line.split("\n")
+    president = tmp_list[0]
+    parties = tmp_list[1:]
+    Slice=tmp_list[2:]
+    for each in Slice:
+        tmp_list.append(each)
+    print(tmp_list)
+        
+President_file.close()
+
+
+csv_file.close()
