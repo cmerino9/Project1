fc4a16a91973374d8dbfdb8a88057e3aa8996017
cmerino9 <57924005+cmerino9@users.noreply.github.com>
2020-04-02 19:44:16 -0400

    Add files via upload

--- Project1-MysteryInc.py
+++ Project1-MysteryInc.py
@@ -4,26 +4,18 @@ Created on Sat Mar 21 08:43:34 2020
 
 @authors: Cortney, Thuy, Shireen, Kazi
 """
-
-'''employmentData = open("TotalPrivateEmployment-data.csv", "r") #data file and program are in same directory4
-lineList = [line.rstrip('\n') for line in open("TotalPrivateEmployment-data.csv")]
-'''
 import csv
 with open('TotalPrivateEmployment-data.csv', mode='r') as csv_file:
-    csv_reader = csv.DictReader(csv_file, delimiter=',')
-    line_count = 0
-    for row in csv_reader:
-        if line_count == 0:
-            print(f'Column names are {", ".join(row)}')
-            line_count += 1
-        print(row)
-        line_count += 1
-    print(f'Processed {line_count} lines.')
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    next(csv_reader)
+    for line in csv_reader:
+        year = line[0]
+        totalJobs = 0
 
 President_file = open('Presidents.txt','r')
-president=[]
-party=[]
-termServed=[]
+president = []
+party = []
+termServed = []
 totalDemocrats = 0
 with open("Presidents.txt", "r") as President_file:
     csv_reader = csv.reader(President_file, delimiter=',')
@@ -33,12 +25,13 @@ with open("Presidents.txt", "r") as President_file:
       termServed = line[2:]
       if party == 'Democrat':
           totalDemocrats += 1
+          
       print(president, "is a", party, "and served", len(termServed), "years.")
       print("Years served are", termServed)
     print("There are", totalDemocrats, "total democrats.")
         
-
-President_file.close()
-
-
 csv_file.close()
+President_file.close()
+               
+
+
